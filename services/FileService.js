5a4ffdb1b1540d5caa91d1e424584d5671bb2835
Yurii-Khrobak <khrobakyuriuwork@gmail.com>
2023-08-24 11:59:41 +0300

    path

--- services/FileService.js
+++ services/FileService.js
@@ -2,16 +2,16 @@ const uuid = require('uuid')
 const path = require('path')
 
 class FileService {
-  saveFile(file) {
-    try {
-      const fileName = uuid.v4() + '.jpg'
-      const filePath = path.resolve('static', fileName)
-      file.mv(filePath)
-      return fileName
-    } catch (e) {
-      throw new Error(e.message)
-    }
-  }
+	saveFile(file) {
+		try {
+			const fileName = uuid.v4() + '.jpg'
+			const filePath = path.join(process.cwd(), fileName)
+			file.mv(filePath)
+			return fileName
+		} catch (e) {
+			throw new Error(e.message)
+		}
+	}
 }
 
 module.exports = new FileService()
